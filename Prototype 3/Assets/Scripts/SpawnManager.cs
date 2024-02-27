fd385f4a55d16da861ff55fd1562356590cab334
DragonKnightMax <84913570+DragonKnightMax@users.noreply.github.com>
2024-02-27 23:12:26 +0800

    Complete Unit 3 - Sound and Effects

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find using tag, get reference to PlayerController script
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        // create obstacle object is not game over
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+      
+    }
+}
